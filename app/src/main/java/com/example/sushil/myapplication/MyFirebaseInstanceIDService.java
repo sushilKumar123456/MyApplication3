32deed218aa3ab24cf7549569d68d4fcdb3b0aa0
Sushil <sushil_kumar180@yahoo.com>
2017-01-26 19:20:35 +0530

    mynes

--- app/src/main/java/com/example/sushil/myapplication/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/example/sushil/myapplication/MyFirebaseInstanceIDService.java
@@ -1,9 +1,7 @@
 package com.example.sushil.myapplication;
 
-import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
