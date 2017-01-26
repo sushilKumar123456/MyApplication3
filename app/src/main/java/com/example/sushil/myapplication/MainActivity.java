05f5a167c2882a4c9a564c562ffc7e531255e923
Sushil <sushil_kumar180@yahoo.com>
2017-01-26 19:00:07 +0530

    my 2

--- app/src/main/java/com/example/sushil/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sushil/myapplication/MainActivity.java
@@ -11,12 +11,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,7 +24,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import Utils.Config;
 import Utils.NotificationUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable {
 
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -132,5 +131,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void run() {
+
     }
 }
